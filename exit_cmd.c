62534360efecf7ff4153f7670ef8bfebfd5a3113
Olatundji <104756391+Olatundji@users.noreply.github.com>
2022-08-13 00:03:26 +0100

    Update exit_cmd.c

--- exit_cmd.c
+++ exit_cmd.c
@@ -1,29 +1,15 @@
-nclude "shell.h"
-
-
+#include "shell.h"
 
 /**
+ * exit_cmd - handles the exit command
+ * @command: tokenized command
+ * @line: input read from stdin
  *
- *  * exit_cmd - handles the exit command
- *
- *   * @command: tokenized command
- *
- *    * @line: input read from stdin
- *
- *     *
- *
- *      * Return: no return
- *
- *       */
-
+ * Return: no return
+ */
 void exit_cmd(char **command, char *line)
-
 {
-
-	  free(line);
-
-	    free_buffers(command);
-
-	      exit(0);
-
+  free(line);
+  free_buffers(command);
+  exit(0);
 }
