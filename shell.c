5f85b5132f3a40da63e1504d092543828858f768
Olatundji <oikpaye@gmail.com>
2022-08-12 07:32:10 -0700

    C-simple_shell

---
+++ shell.c
@@ -0,0 +1,33 @@
+nclude "main.h"
+
+
+
+/**
+ *
+ *  * main - func to start simple shell
+ *
+ *   * @ac: No use
+ *
+ *    * @av: No use
+ *
+ *     * Return: loop.
+ *
+ *      **/
+
+int main(int ac, char **av)
+
+{
+
+		(void)av;
+
+			(void)ac;
+
+
+
+				signal(SIGINT, run_shell);
+
+					prompt();
+
+						return (0);
+
+}
