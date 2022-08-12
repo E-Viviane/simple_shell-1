da4316cfa05c5de469023af21938264cd52b54b3
Olatundji <104756391+Olatundji@users.noreply.github.com>
2022-08-12 16:10:04 +0100

    Update parser.c

--- parser.c
+++ parser.c
@@ -1,75 +1,38 @@
-nclude "main.h"
-
-
+#include "main.h"
 
 /**
- *
- *  * main - func to start simple shell
- *
- *   * @argc: argument coun
- *
- *    * @argv: argument vector
- *
- *     * Return: simple shell
- *
- *      **/
-
+ * main - func to start simple shell
+ * @argc: argument coun
+ * @argv: argument vector
+ * Return: simple shell
+ **/
 int main(int argc, char **argv)
-
 {
+	(void)argc;
+	(void)argv;
 
-		(void)argc;
-
-			(void)argv;
-
-
-
-				run_prompt();
-
-					return (0);
-
+	run_prompt();
+	return (0);
 }
 
-
-
 /**
+ * run_prompt - call prompt to start shell input
  *
- *  * run_prompt - call prompt to start shell input
- *
- *   *
- *
- *    **/
-
+ **/
 void run_prompt(void)
-
 {
+	char *prompt;
+	char **cmd_args;
 
-		char *prompt;
+	for (;;)
+	{
+		_stream_writer("$ ");
+		prompt = read_prompt();
+		cmd_args = tokenize(prompt);
 
-			char **cmd_args;
+		exec_cmd(cmd_args);
+	}
 
-
-
-				for (;;)
-
-						{
-
-									_stream_writer("$ ");
-
-											prompt = read_prompt();
-
-													cmd_args = tokenize(prompt);
-
-
-
-															exec_cmd(cmd_args);
-
-																}
-
-
-
-					free(prompt);
-
-						free(cmd_args);
-
+	free(prompt);
+	free(cmd_args);
 }
