65bfb2178712744d432e376b0d8154747fbf7bde
Olatundji <104756391+Olatundji@users.noreply.github.com>
2022-08-13 00:25:47 +0100

    Update generate_authors.sh

--- generate_authors.sh
+++ generate_authors.sh
@@ -1,23 +1,13 @@
-et -e
-
+#!/usr/bin/env bash
+set -e
 cd "$(dirname "$(readlink -f "$BASH_SOURCE")")/.."
 
-
-
 # see also ".mailmap" for how email addresses and names are deduplicated
-
 {
-
-		cat <<-'EOH'
-
+	cat <<-'EOH'
 	# This file lists all individuals having contributed content to the repository.
-
 	# For how it is generated, see `scripts/generate-authors.sh`.
-
 	EOH
-
-			echo
-
-						git log --format='%aN <%aE>' | LC_ALL=C.UTF-8 sort -uf
-
-								} > AUTHORS
+		echo
+			git log --format='%aN <%aE>' | LC_ALL=C.UTF-8 sort -uf
+		} > AUTHORS
