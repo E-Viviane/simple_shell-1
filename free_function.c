3f8e12704229a98f06f654cbe1c4a6f9b3de5ddf
Olatundji <104756391+Olatundji@users.noreply.github.com>
2022-08-13 00:02:27 +0100

    Update free_function.c

--- free_function.c
+++ free_function.c
@@ -1,41 +1,21 @@
-nclude "shell.h"
-
-
+#include "shell.h"
 
 /**
+ * free_buffers - frees buffers
+ * @buf: buffer to be freed
  *
- *  * free_buffers - frees buffers
- *
- *   * @buf: buffer to be freed
- *
- *    *
- *
- *     * Return: no return
- *
- *      */
-
+ * Return: no return
+ */
 void free_buffers(char **buf)
-
 {
+  int i = 0;
 
-	  int i = 0;
-
-
-
-	    if (!buf || buf == NULL)
-
-		        return;
-
-	      while (buf[i])
-
-		          {
-
-				        free(buf[i]);
-
-					      i++;
-
-					          }
-
-	        free(buf);
-
+  if (!buf || buf == NULL)
+    return;
+  while (buf[i])
+    {
+      free(buf[i]);
+      i++;
+    }
+  free(buf);
 }
